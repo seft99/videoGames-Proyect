2e099515b88f539c7f52dbd7ddff29d1ff84ea0e
seft99 <sebas.1910@hotmail.com>
2023-11-22 17:09:09 -0500

    registrar y rutas

--- src/app/page/register-user/register-user.component.ts
+++ src/app/page/register-user/register-user.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+// register-user.component.ts
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ModalService } from 'src/assets/services/modal-service.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register-user',
   templateUrl: './register-user.component.html',
-  styleUrls: ['./register-user.component.scss']
+  styleUrls: ['./register-user.component.scss'],
 })
-export class RegisterUserComponent {
+export class RegisterUserComponent implements OnInit {
+  blurState = false;
+  form! : FormGroup
 
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialogRef: MatDialogRef<RegisterUserComponent>,
+    private modalService: ModalService,
+    private fb : FormBuilder
+  ) {
+
+    this.form = this.fb.group({
+      userName:['',Validators.required],
+      email: ['', [Validators.required , Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
+      password: ['', Validators.required]
+    })
+  }
+
+  ngOnInit(): void {
+    this.modalService.blurState$.subscribe(blur => {
+      this.blurState = blur;
+    });
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
--- src/app/shared/material-imports/material-imports.module.ts
+++ src/app/shared/material-imports/material-imports.module.ts
@@ -4,10 +4,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
 import {MatMenuModule} from '@angular/material/menu';
-
+import {MatDialogModule} from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatIconModule, MatToolbarModule,MatCardModule,MatMenuModule],
-  exports: [MatIconModule, MatToolbarModule,MatCardModule,MatMenuModule],
+  imports: [CommonModule, MatIconModule, MatToolbarModule,MatCardModule,MatMenuModule,MatDialogModule,MatInputModule],
+  exports: [MatIconModule, MatToolbarModule,MatCardModule,MatMenuModule,MatDialogModule,MatInputModule],
 })
 export class MaterialImportsModule {}
